a229ae61216fbe257762ee657d60d9d3e938a11e
Sappo <kevinsapper88@gmail.com>
2013-11-21 10:03:48 +0100

    added error support; added checksum support; fixed freeing bug

--- include/msg.h
+++ include/msg.h
@@ -30,11 +30,16 @@ extern "C" {
 
 // Message commands 
 #define ZS_CMD_LAST_STATE 0x1
-#define ZS_CMD_FILE_LIST 0x2
+#define ZS_CMD_UPDATE 0x2
 #define ZS_CMD_NO_UPDATE 0x3
 #define ZS_CMD_REQUEST_FILES 0x4
 #define ZS_CMD_GIVE_CREDIT 0x5
 #define ZS_CMD_SEND_CHUNK 0x6 
+#define ZS_CMD_ABORT 0x7
+
+#define ZS_FILE_OP_UPD 0x1
+#define ZS_FILE_OP_DEL 0x2
+#define ZS_FILE_OP_REN 0x3
 
 // Opaque class structure
 typedef struct _zs_msg_t zs_msg_t;
@@ -49,136 +54,137 @@ typedef uint16_t string_size_t;
 // @interface
 // Contructor, creates new zs message
 zs_msg_t *
-zs_msg_new (int cmd);
+    zs_msg_new (int cmd);
 
 // Destructor, destroys zs message
 void
-zs_msg_destroy (zs_msg_t **self_p);
+    zs_msg_destroy (zs_msg_t **self_p);
 
 // Constructor, creates new zs file meta data
 zs_fmetadata_t *
-zs_fmetadata_new ();
+    zs_fmetadata_new ();
 
 // Destructor, destroys file meta data
 void
-zs_fmetadata_destroy (zs_fmetadata_t **self_p);
+    zs_fmetadata_destroy (zs_fmetadata_t **self_p);
 
 // --------------------------------------------------------------------------
 // Receive & Send
 
 // receive messages
 zs_msg_t *
-zs_msg_recv (void *input);
+    zs_msg_recv (void *input);
 
 // send LAST_STATE
 int 
-zs_msg_send_last_state (void *output, uint64_t state);
-
+    zs_msg_send_last_state (void *output, uint64_t state);
  
 // send FILE_LIST
 int
-zs_msg_send_file_list (void *output, zlist_t *filemeta_list);
+    zs_msg_send_update (void *output, uint64_t state, zlist_t *filemeta_list);
  
 // send NO_UPDATE
 int
-zs_msg_send_no_update (void *output);
+    zs_msg_send_no_update (void *output);
  
 // send REQUEST_FILES
 int
-zs_msg_send_request_files (void *output, zlist_t *fpaths);
+    zs_msg_send_request_files (void *output, zlist_t *fpaths);
 
 // send GIVE CREDIT
 int
-zs_msg_send_give_credit (void *output, uint64_t credit);
+    zs_msg_send_give_credit (void *output, uint64_t credit);
 
 // send SEND CHUNK
 int
-zs_msg_send_chunk (void *output, uint64_t sequence, char *file_path, uint64_t offset, zframe_t *chunk);
+    zs_msg_send_chunk (void *output, uint64_t sequence, char *file_path, uint64_t offset, zframe_t *chunk);
+
+
 // --------------------------------------------------------------------------
 // zs_msg_t get & set
 
 // getter/setter message command    
 int
-zs_msg_get_cmd (zs_msg_t *self);
+    zs_msg_get_cmd (zs_msg_t *self);
 
 // getter/setter message state    
-void 
-zs_msg_set_state (zs_msg_t *self, uint64_t state);
+void
+    zs_msg_set_state (zs_msg_t *self, uint64_t state);
 
-uint64_t 
-zs_msg_get_state (zs_msg_t *self);
+uint64_t
+    zs_msg_get_state (zs_msg_t *self);
 
 // getter/setter message file meta list
-void 
-zs_msg_set_fmetadata (zs_msg_t *self, zlist_t *filemeta_list);
+void
+    zs_msg_set_fmetadata (zs_msg_t *self, zlist_t *filemeta_list);
 
 zlist_t *
-zs_msg_get_fmetadata (zs_msg_t *self);
+    zs_msg_get_fmetadata (zs_msg_t *self);
 
 // Iterate file meta data
 
 zs_fmetadata_t *
-zs_msg_fmetadata_first (zs_msg_t *self);
+    zs_msg_fmetadata_first (zs_msg_t *self);
 
 zs_fmetadata_t *
-zs_msg_fmetadata_next (zs_msg_t *self);
+    zs_msg_fmetadata_next (zs_msg_t *self);
 
 void
-zs_msg_fmetadata_append (zs_msg_t *self, zs_fmetadata_t *fmetadata_item);
+    zs_msg_fmetadata_append (zs_msg_t *self, zs_fmetadata_t *fmetadata_item);
 
 // getter/setter message file paths
 
-void 
-zs_msg_set_fpaths (zs_msg_t *self, zlist_t *fpaths);
+void
+    zs_msg_set_fpaths (zs_msg_t *self, zlist_t *fpaths);
 
 zlist_t *
-zs_msg_get_fpaths (zs_msg_t *self);
+    zs_msg_get_fpaths (zs_msg_t *self);
 
 // Iterate fpaths
 
 char *
-zs_msg_fpaths_first (zs_msg_t *self);
+    zs_msg_fpaths_first (zs_msg_t *self);
 
 char *
-zs_msg_fpaths_next (zs_msg_t *self);
+    zs_msg_fpaths_next (zs_msg_t *self);
 
 void
-zs_msg_fpaths_append (zs_msg_t *self, char *format, ...);
+    zs_msg_fpaths_append (zs_msg_t *self, char *format, ...);
 
 // getter/setter message credit
 void
-zs_msg_set_credit (zs_msg_t *self, uint64_t credit);
+    zs_msg_set_credit (zs_msg_t *self, uint64_t credit);
 
 uint64_t
-zs_msg_get_credit (zs_msg_t *self);
+    zs_msg_get_credit (zs_msg_t *self);
 
 // getter/setter message chunk
 void
-zs_msg_set_chunk (zs_msg_t *self, zframe_t *chunk);
+    zs_msg_set_chunk (zs_msg_t *self, zframe_t *chunk);
 
-zframe_t* 
-zs_msg_get_chunk (zs_msg_t *self);
+zframe_t *
+    zs_msg_get_chunk (zs_msg_t *self);
 
 // getter/setter message sequence
 void
-zs_msg_set_sequence (zs_msg_t *self, uint64_t seq);
+    zs_msg_set_sequence (zs_msg_t *self, uint64_t seq);
 
 uint64_t
-zs_msg_get_sequence (zs_msg_t *self);
+    zs_msg_get_sequence (zs_msg_t *self);
 
 // getter/setter message file-path
 void
-zs_msg_set_file_path (zs_msg_t *self, char *format, ...);
+    zs_msg_set_file_path (zs_msg_t *self, char *format, ...);
 
-char*
-zs_msg_get_file_path (zs_msg_t *self);
+char *
+    zs_msg_get_file_path (zs_msg_t *self);
 
 // getter/setter message offset
 void
-zs_msg_set_offset (zs_msg_t *self, uint64_t offset);
+    zs_msg_set_offset (zs_msg_t *self, uint64_t offset);
 
 uint64_t
-zs_msg_get_offset (zs_msg_t *self);
+    zs_msg_get_offset (zs_msg_t *self);
 
 
 // --------------------------------------------------------------------------
@@ -187,24 +193,38 @@ zs_msg_get_offset (zs_msg_t *self);
 
 // getter/setter file path
 void
-zs_fmetadata_set_path (zs_fmetadata_t *self, char* format, ...);
+    zs_fmetadata_set_path (zs_fmetadata_t *self, char* format, ...);
 
 char *
-zs_fmetadata_get_path (zs_fmetadata_t *self);
+    zs_fmetadata_get_path (zs_fmetadata_t *self);
+
+// getter/setter file operation
+void
+    zs_fmetadata_set_operation(zs_fmetadata_t *self, int operation);
+     
+int
+    zs_fmetadata_get_operation(zs_fmetadata_t *self);
 
 // getter/setter file size
 void
-zs_fmetadata_set_size (zs_fmetadata_t *self, uint64_t size);
+    zs_fmetadata_set_size (zs_fmetadata_t *self, uint64_t size);
 
 uint64_t 
-zs_fmetadata_get_size (zs_fmetadata_t *self);
+    zs_fmetadata_get_size (zs_fmetadata_t *self);
 
 // getter/setter timestamp
 void
-zs_fmetadata_set_timestamp (zs_fmetadata_t *self, uint64_t timestamp);
+    zs_fmetadata_set_timestamp (zs_fmetadata_t *self, uint64_t timestamp);
 
 uint64_t
-zs_fmetadata_get_timestamp (zs_fmetadata_t *self);
+    zs_fmetadata_get_timestamp (zs_fmetadata_t *self);
+
+// getter/setter checksum
+void
+    zs_fmetadata_set_checksum (zs_fmetadata_t *self, uint64_t checksum);
+
+uint64_t
+    zs_fmetadata_get_checksum (zs_fmetadata_t *self);
 
 // Self test this class
 int
