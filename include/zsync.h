7912f5967809064176c808088af62e1a76e988b3
sappo <kevinsapper88@gmail.com>
2014-01-28 14:40:12 +0100

    fixed wrapper header class

--- include/zsync.h
+++ include/zsync.h
@@ -31,10 +31,10 @@
 #define ZSYNC_VERSION_MINOR 0
 #define ZSYNC_VERSION_PATCH 0
 
-#define CZMQ_MAKE_VERSION(major, minor, patch) \
+#define ZSYNC_MAKE_VERSION(major, minor, patch) \
     ((major) * 10000 + (minor) * 100 + (patch))
-#define CZMQ_VERSION \
-    CZMQ_MAKE_VERSION(CZMQ_VERSION_MAJOR, CZMQ_VERSION_MINOR, CZMQ_VERSION_PATCH)
+#define ZSYNC_VERSION \
+    ZSYNC_MAKE_VERSION(ZSYNC_VERSION_MAJOR, ZSYNC_VERSION_MINOR, ZSYNC_VERSION_PATCH)
 
 //  Classes in the API
 
