181a5cfb9c7a3382bdb2175eada3d719ec2264cc
sappo <kevinsapper88@gmail.com>
2014-02-25 10:43:59 +0100

    updated man pages and install script

--- install-zsync-dep.sh
+++ install-zsync-dep.sh
@@ -1,17 +1,29 @@
-# ZeroSync
+#!/bin/bash
+set -e
+set -o
+
+CONFIGURE_HOST_32="i686-linux-gnu"
+CONFIGURE_FLAG_32="-m32"
+
+# ZeroSync 64-bit install
 
 mkdir build
 cd build
 
 #   Build required ZeroSync projects first
-sudo apt-get install uuid-dev
+sudo apt-get install g++ git automake autoconf
 
 #   libsodium
 git clone git://github.com/jedisct1/libsodium.git
 cd libsodium
 ./autogen.sh
-./configure && make check
-sudo make install
+if [[ $HOST == "32" ]]; then
+    ./configure --host=$CONFIGURE_HOST_32 "CFLAGS=$CONFIGURE_FLAG_32" "CXXFLAGS=$CONFIGURE_FLAG_32" "LDFLAGS=$CONFIGURE_FLAG_32"
+else
+    ./configure
+fi
+make check
+sudo make install # libdir=absolutePath includedir=absolutePath
 sudo ldconfig
 cd ..
 
@@ -19,7 +31,12 @@ cd ..
 git clone git://github.com/zeromq/libzmq.git
 cd libzmq
 ./autogen.sh
-./configure && make check
+if [[ $HOST == "32" ]]; then
+    ./configure --host=$CONFIGURE_HOST_32 "CFLAGS=$CONFIGURE_FLAG_32" "CXXFLAGS=$CONFIGURE_FLAG_32" "LDFLAGS=$CONFIGURE_FLAG_32"
+else
+    ./configure
+fi
+make check
 sudo make install
 sudo ldconfig
 cd ..
@@ -28,20 +45,30 @@ cd ..
 git clone git://github.com/zeromq/czmq.git
 cd czmq
 ./autogen.sh
-./configure && make check
+if [[ $HOST == "32" ]]; then
+    ./configure --host=$CONFIGURE_HOST_32 "CFLAGS=$CONFIGURE_FLAG_32" "CXXFLAGS=$CONFIGURE_FLAG_32" "LDFLAGS=$CONFIGURE_FLAG_32"
+else
+    ./configure
+fi
+make check
 sudo make install
 sudo ldconfig
 cd ..
 
 #   Zyre
 git clone git://github.com/zeromq/zyre.git
-#cd zyre
-#./autogen.sh
-#./configure && make check
-#sudo make install
-#sudo ldconfig
-#cd ..
+cd zyre
+./autogen.sh
+if [[ $HOST == "32" ]]; then
+    ./configure --host=$CONFIGURE_HOST_32 "CFLAGS=$CONFIGURE_FLAG_32" "CXXFLAGS=$CONFIGURE_FLAG_32" "LDFLAGS=$CONFIGURE_FLAG_32"
+else
+    ./configure
+fi
+make check
+sudo make install
+sudo ldconfig
+cd ..
 
 #   Build and check libzsync
-#script: ./autogen.sh && ./configure && make # && make check
+script: ./autogen.sh && ./configure && make # && make check
 
